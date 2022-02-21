1a25caa4b9d0c6e7ce59a102a361e37f67ec82c8
Alfarizi2103 <alfariziaries2103@gmail.com>
2022-02-21 19:29:30 +0700

    membuat login multi role dan register akun

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\BerandaController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,26 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+//route login
+Route::get('/login', [LoginController::class,'halamanlogin'])->name('login');
+Route::post('/postlogin', [LoginController::class,'postlogin'])->name('postlogin');
+Route::get('/logout', [LoginController::class,'logout'])->name('logout');
+route::get('/registrasi',[LoginController::class,'registrasi'])->name('registrasi');
+route::post('/simpanregistrasi',[LoginController::class,'simpanregistrasi'])->name('simpanregistrasi');
+
+//route role admin
+Route::group(['middleware' => ['auth','ceklevel:admin']], function () {
+    Route::get('/halaman-satu', [BerandaController::class,'halamansatu'])->name('halaman-satu');    
+});
+
+//route role user
+Route::group(['middleware' => ['auth','ceklevel:admin,user,pegawai']], function () {
+    Route::get('/beranda', [BerandaController::class,'index']);
+    Route::get('/halaman-dua', [BerandaController::class,'halamandua'])->name('halaman-dua');
+});
+
+//route role pegawai
+Route::group(['middleware' => ['auth','ceklevel:admin,pegawai']], function () {
+    Route::get('/halaman-tiga', [BerandaController::class,'halamantiga'])->name('halaman-tiga');
 });
